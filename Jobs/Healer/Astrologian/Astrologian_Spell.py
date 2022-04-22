3c90a3dee0d7ca08d07b16e6b0a30298d6c3e2a9
Anthony Desrochers <anthony.desrochers17@gmail.com>
2022-04-22 01:03:40 -0400

    Added Astrologian
    Still Missing Earthly Star

---
+++ Jobs/Healer/Astrologian/Astrologian_Spell.py
@@ -0,0 +1,183 @@
+import copy
+
+from Jobs.Base_Spell import DOTSpell, ManaRequirement, empty
+from Jobs.Healer.Healer_Spell import AstrologianSpell
+Lock = 0.75
+
+#Requirement
+
+def DrawRequirement(Player, Spell):
+    return Player.DrawStack > 0
+
+def ArcanumRequirement(Player, Spell):
+    return Player.HasCard
+
+def MinorArcanaRequirement(Player, Spell):
+    return Player.MinorArcanaCD <= 0
+
+def LordOfCrownRequirement(Player, Spell):
+    return Player.LordOfCrown #Will be assumed to be given by Minor Arcana
+
+def DivinationRequirement(Player, Spell):
+    return Player.DivinationCD <= 0
+
+def LightspeedRequirement(Player, Spell):
+    return Player.LightspeedCD <= 0
+
+#Apply
+
+def ApplyAstrodyne(Player, Enemy):
+    check = 0
+
+    if Player.Lunar:
+        check +=1
+        Player.Lunar = False
+    if Player.Solar:
+        check +=1
+        Player.Solar = False
+    if Player.Celestial:
+        check +=1
+        Player.Celestial = False
+    #Will only consider Body and Mind
+    if check >= 2:
+        Player.EffectList.append(BodyEffect)
+        Player.EffectCDList.append(BodyCheck)#Only 1 check for both Body and Mind since same Timer
+        Player.BodyTimer = 15
+    if check == 3:
+        Player.MultDPSBonus *= 1.05
+        Player.EffectCDList.append(MindCheck)
+
+
+def ApplyDraw(Player, Enemy):
+    Player.HasCard = True
+
+    if Player.DrawStack == 2:
+        Player.EffectCDList.append(DrawStackCheck)
+        Player.DrawCD = 30
+    Player.DrawStack -= 1
+
+def ApplyMinorArcana(Player, Enemy):
+    Player.LordOfCrown = True
+    Player.MinorArcanaCD = 60
+
+def ApplyLordOfCrown(Player, Enemy):
+    Player.LordOfCrown = False
+
+def ApplyDivination(Player, Enemy):
+    Enemy.Bonus *= 1.06 #Just give DPS bonus on Enemy instead of raid wide buff
+    Player.DivinationCD = 120
+    Player.DivinationTimer = 15
+    Player.EffectCDList.append(DivinationCheck)
+
+def ApplyLightspeed(Player, Enemy):
+    Player.LightspeedTimer = 15
+    Player.LightspeedCD = 90
+    Player.EffectList.append(LightspeedEffect)
+    Player.EffectCDList.append(LightspeedCheck)
+
+def ApplyCombust(Player, Enemy):
+    if Player.CumbustDOT == None:
+        Player.CumbustDOT = copy.deepcopy(CumbustDOT)
+        Player.CumbustDOTTimer = 30
+        Player.DOTList.append(Player.CumbustDOT)
+        Player.EffectCDList.append(CumbustDOTCheck)
+    Player.CumbustTimer = 30
+
+
+#Effect
+
+def BodyEffect(Player, Spell):
+    if Spell.id == Malefic.id or Spell.id == Combust.id : #Only two affected spells for now
+        Spell.CastTime *= 0.9
+        Spell.RecastTime *= 0.9
+
+def LightspeedEffect(Player, Spell):
+    if Spell.GCD: Spell.CastTime = max(Lock, Spell.CastTime - 2.5)
+
+#Check
+
+def BodyCheck(Player, Enemy):
+    if Player.BodyTimer <= 0:
+        Player.EffectList.remove(BodyEffect)
+        Player.EffectToRemove.append(BodyCheck)
+
+def MindCheck(Player, Enemy):
+    if Player.BodyTimer <= 0:
+        Player.MultDPSBonus /= 1.05
+        Player.EffectToRemove.append(MindCheck)
+
+
+
+
+def DrawStackCheck(Player, Enemy):
+    if Player.DrawCD <= 0:
+        if Player.DrawStack == 1:
+            Player.EffectToRemove.append(DrawStackCheck)
+        else:
+            Player.DrawCD = 30
+        Player.DrawStack += 1
+
+def DivinationCheck(Player, Enemy):
+    if Player.DivinationTimer <= 0:
+        Enemy.Bonus /= 1.06
+        Player.EffectToRemove.append(DivinationCheck)
+
+def LightspeedCheck(Player, Enemy):
+    if Player.LightspeedTimer <= 0:
+        Player.EffectList.remove(LightspeedEffect)
+        Player.EffectToRemove.append(LightspeedCheck)
+
+
+def CumbustDOTCheck(Player, Enemy):
+    if Player.CumbustDOTTimer <= 0:
+        Player.DOTList.remove(Player.CumbustDOT)
+        Player.CumbustDOT = None
+        Player.EffectToRemove.append(CumbustDOTCheck)
+
+#GCD
+Malefic = AstrologianSpell(1, True, 1.5, 2.5, 250, 400, empty, [ManaRequirement])
+Combust = AstrologianSpell(2, True, Lock, 2.5, 0, 400, ApplyCombust, [ManaRequirement])
+LordOfCrown = AstrologianSpell(5, True, Lock, 1, 250, 0,  ApplyLordOfCrown, [LordOfCrownRequirement])
+CumbustDOT = DOTSpell(-12, 55)
+
+
+#oGCD
+Lightspeed = AstrologianSpell(3, False, Lock, 0, 0, 0, ApplyLightspeed, [LightspeedRequirement])
+Divination = AstrologianSpell(4, False, Lock, 0, 0, 0, ApplyDivination, [DivinationRequirement])
+MinorArcana = AstrologianSpell(5, False, Lock, 0, 0, 0, ApplyMinorArcana, [MinorArcanaRequirement])
+Draw = AstrologianSpell(6, False, Lock,0, 0, 0, ApplyDraw, [DrawRequirement])
+Astrodyne = AstrologianSpell(7, False, Lock, 0, 0, 0, ApplyAstrodyne, [])
+
+#Arcanum require a target within the team, so it will be a function that will return a spell that
+#will target the given player. It is also assumed that the bonus is 6%
+
+
+
+def Arcanum(Target, Type):
+    #Target is the player object to which we will apply the buff
+    #Type will specify which Astrosign
+
+    def ArcanumCheck(Player, Enemy):
+        if Player.ArcanumTimer <= 0:
+            #input("Effect has been removed on : " + str(Player))
+            Player.EffectToRemove.append(ArcanumCheck)
+            Player.MultDPSBonus /= 1.06
+        pass #This function is just to know if the Target has already been given a buff
+
+    def ApplyArcanum(Player, Spell):
+        Player.HasCard = False
+        if Target.ArcanumTimer == 0 : #Can only have 1 buff at a time
+            #input("Will affect target with Arcana :" + str(Target))
+            Target.MultDPSBonus *= 1.06
+            Target.EffectCDList.append(ArcanumCheck)
+
+        Target.ArcanumTimer = 15
+
+        if Type == "Lunar" : Player.Lunar = True
+        elif Type == "Solar" : Player.Solar = True
+        elif Type == "Celestial" : Player.Celestial = True
+
+
+    return AstrologianSpell(0, False, Lock, 0, 0, 0, ApplyArcanum, [ArcanumRequirement])
+
+        
--- main.py
+++ main.py
@@ -2,22 +2,26 @@ from Enemy import *
 from Fight import *
 
 from Jobs.Base_Spell import WaitAbility, Potion
-
+#CASTER
 from Jobs.Caster.Caster_Spell import SwiftCast
 from Jobs.Caster.Blackmage.BlackMage_Spell import * 
 from Jobs.Caster.Redmage.Redmage_Spell import *
 from Jobs.Caster.Blackmage.BlackMage_Player import * 
 from Jobs.Caster.Redmage.Redmage_Player import *
 
+#HEALER
 from Jobs.Healer.Scholar.Scholar_Spell import *
 from Jobs.Healer.Whitemage.Whitemage_Spell import *
+from Jobs.Healer.Astrologian.Astrologian_Spell import *
 from Jobs.Healer.Scholar.Scholar_Player import *
 from Jobs.Healer.Whitemage.Whitemage_Player import *
+from Jobs.Healer.Astrologian.Astrologian_Player import *
 
+#RANGED
 from Jobs.Ranged.Machinist.Machinist_Spell import *
 from Jobs.Ranged.Machinist.Machinist_Player import *
 
-
+#TANK
 from Jobs.Tank.Gunbreaker.Gunbreaker_Spell import *
 from Jobs.Tank.DarkKnight.DarkKnight_Spell import *
 from Jobs.Tank.Warrior.Warrior_Spell import *
@@ -27,7 +31,7 @@ from Jobs.Tank.DarkKnight.DarkKnight_Player import *
 from Jobs.Tank.Warrior.Warrior_Player import *
 from Jobs.Tank.Paladin.Paladin_Player import *
 
-
+#MELEE
 from Jobs.Melee.Samurai.Samurai_Spell import *
 from Jobs.Melee.Ninja.Ninja_Spell import *
 from Jobs.Melee.Samurai.Samurai_Player import *
@@ -48,13 +52,28 @@ WHMStat = {"MainStat": 2571, "WD":120, "Det" : 1830, "Ten" : 400, "SS": 489, "Cr
 SAMStat = {"MainStat": 2563, "WD":120, "Det" : 1654, "Ten" : 400, "SS": 579, "Crit" : 2310, "DH" : 1217}
 PLDStat = {"MainStat": 2502, "WD":120, "Det" : 1680, "Ten" : 527, "SS": 650, "Crit" : 2319, "DH" : 1012}
 GNBStat = {"MainStat": 2517, "WD":120, "Det" : 1612, "Ten" : 527, "SS": 950, "Crit" : 2205, "DH" : 868}
+ASTStat = {"MainStat": 2560, "WD":120, "Det" : 1951, "Ten" : 400, "SS": 716, "Crit" : 2277, "DH" : 844}
+
 
 Event = Fight([], Dummy)
 
 #DRKPlayer = DarkKnight(2.41, DRKAction, [], [DarksideEffect], Event)
 
+BLMPlayer = BlackMage(2.5, [], [], [AstralFire, UmbralIce,UmbralHeartEffect], Event, BLMStat)
+SCHPlayer = Scholar(2.5, [], [], [], Event, SCHStat)
+RDMPlayer = Redmage(2.5, [], [], [DualCastEffect], Event, RDMStat)
+MCHPlayer = Machinist(2.5, [], [], [], Event, MCHStat)
+NINPlayer = Ninja(2.5, [], [], [], Event, NINStat)
+DRKPlayer = DarkKnight(2.5, [], [], [], Event, DRKStat)
+WARPlayer = Warrior(2.5, [], [], [SurgingTempestEffect], Event, WARStat)
+WHMPlayer = Whitemage(2.5, [], [], [], Event, WHMStat)
+SAMPlayer = Samurai(2.5, [], [], [], Event, SAMStat)
+PLDPlayer = Paladin(2.5, [], [], [], Event, PLDStat)
+GNBPlayer = Gunbreaker(2.5, [], [], [], Event, GNBStat)
+ASTPlayer = Astrologian(2.5, [], [], [], Event, ASTStat)
+
 BLMOpener = [Sharp, WaitAbility(16.5), F3, T3, F4, Triple, F4, Potion, F4, Amp, Ley, F4, Triple, Despair, Mana, F4, SwiftCast, Despair, Transpo, Para, Xeno, T3, F3, F4, F4, F4, Despair, Xeno, Transpo, Para]
-SCHOpener = [WaitAbility(17), Potion, WaitAbility(1), Broil, Biolysis, Aetherflow, Broil, SwiftCast, Broil, EnergyDrain, Broil, EnergyDrain, Broil, EnergyDrain, Broil, Dissipation, Broil, EnergyDrain, Broil, EnergyDrain, Broil, EnergyDrain, Broil, Broil, Broil, Broil, Broil, Broil, Broil]
+SCHOpener = [WaitAbility(17), Potion, WaitAbility(1), Broil, Biolysis, Aetherflow, Broil, SwiftCast, Broil, ChainStratagem, EnergyDrain, Broil, EnergyDrain, Broil, EnergyDrain, Broil, Dissipation, Broil, EnergyDrain, Broil, EnergyDrain, Broil, EnergyDrain, Broil, Broil, Broil, Broil, Broil, Broil, Broil]
 RDMOpener = [WaitAbility(15), Verthunder, Verareo, SwiftCast, Acceleration, Verthunder, Potion, Verthunder, Embolden, Manafication, Riposte, Fleche, Zwerchhau, Contre, Redoublement, Corps, Engagement, Verholy, Corps, Engagement, Scorch, Resolution, Verfire, Verthunder, Verstone, Verareo, Verfire, Verthunder,Verfire, Verthunder,Verfire,Fleche]
 MCHOpener = [WaitAbility(15), Reassemble, WaitAbility(2.25), Potion, WaitAbility(1.5), AirAnchor, GaussRound, Ricochet, Drill, BarrelStabilizer, SplitShot, SlugShot, GaussRound, Ricochet, CleanShot, Reassemble, WaitAbility(1), Wildfire, ChainSaw, Automaton,WaitAbility(1), Hypercharge, HeatBlast, Ricochet,HeatBlast,GaussRound,HeatBlast,Ricochet,HeatBlast,GaussRound,HeatBlast,Ricochet, Drill, SplitShot,GaussRound, SlugShot,Ricochet,CleanShot,GaussRound,SplitShot, SlugShot,CleanShot,SlugShot]
 NINOpener = [WaitAbility(18.5), Suiton, Kassatsu, SpinningEdge, Potion, GustSlash, Mug, Bunshin, AeolianEdge, WaitAbility(1.75), TrickAttack, Kamaitachi, Hyosho, Raiton, TenChiJin, Meisui, Raiju, Bhavacakra,Raiju, Bhavacakra, SpinningEdge, Raiton, Raiju]
@@ -64,19 +83,23 @@ WHMOpener = [WaitAbility(17), Potion, WaitAbility(1), Glare, Dia, Glare, Glare, 
 SAMOpener = [WaitAbility(11), Meikyo, WaitAbility(8.25), Gekko, Potion, Kasha, Ikishoten, Yukikaze,Shinten, Midare,Shinten, Kaeshi, Senei, Meikyo, Gekko, Higanbana, Kasha, OgiNamikiri, KaeshiNamikiri, Shoha, Gekko, Shinten, Hakaze, Yukikaze, Midare, Kaeshi, Hakaze, Yukikaze, Shinten,Hakaze, Jinpu]
 PLDOpener = [WaitAbility(20), FastBlade, FightOrFlight, RiotBlade, GoringBlade, FastBlade, Potion, RiotBlade, CircleScorn, Intervene, RoyalAuthority, Expiacion, RequestACat, Atonement, Intervene, Atonement, Atonement, FastBlade, RiotBlade, GoringBlade, HolySpirit, HolySpirit, HolySpirit, HolySpirit, Confetti, WaitAbility(1.75),CircleScorn, BladeFaith, WaitAbility(1.75), Expiacion,BladeTruth, BladeValor ]
 GNBOpener = [WaitAbility(20), KeenEdge, BrutalShell, Potion, SolidBarrel, NoMercy, GnashingFang, Bloodfest, JugularRip, DoubleDown, BlastingZone, BowShock, SonicBreak, RoughDivide, SavageClaw, AbdomenTear, RoughDivide, WickedTalon, EyeGouge, BurstStrike, Hypervelocity, KeenEdge, BrutalShell, SolidBarrel,KeenEdge, BrutalShell, SolidBarrel, GnashingFang, JugularRip, SavageClaw, AbdomenTear, WickedTalon,BlastingZone, EyeGouge, BurstStrike, Hypervelocity ]
+ASTOpener = [WaitAbility(17.5), Potion, Malefic, Lightspeed, Combust, Arcanum(WHMPlayer, "Solar"), Draw, Malefic, Arcanum(WHMPlayer, "Lunar"), Draw, Malefic, Divination, Arcanum(WHMPlayer, "Celestial"), Malefic, MinorArcana, Astrodyne, Malefic, LordOfCrown, Malefic, Malefic, Malefic, Malefic, Malefic, Malefic, Malefic, Malefic, Malefic, Malefic, Malefic, Malefic, Malefic, Malefic ]
 
-BLMPlayer = BlackMage(2.5, BLMOpener, [], [AstralFire, UmbralIce,UmbralHeartEffect], Event, BLMStat)
-SCHPlayer = Scholar(2.5, SCHOpener, [], [], Event, SCHStat)
-RDMPlayer = Redmage(2.5, RDMOpener, [], [DualCastEffect], Event, RDMStat)
-MCHPlayer = Machinist(2.5, MCHOpener, [], [], Event, MCHStat)
-NINPlayer = Ninja(2.5, NINOpener, [], [], Event, NINStat)
-DRKPlayer = DarkKnight(2.5, DRKOpener, [], [], Event, DRKStat)
-WARPlayer = Warrior(2.5, WAROpener, [], [SurgingTempestEffect], Event, WARStat)
-WHMPlayer = Whitemage(2.5, WHMOpener, [], [], Event, WHMStat)
-SAMPlayer = Samurai(2.5, SAMOpener, [], [], Event, SAMStat)
-PLDPlayer = Paladin(2.5, PLDOpener, [], [], Event, PLDStat)
-GNBPlayer = Gunbreaker(2.5, GNBOpener, [], [], Event, GNBStat)
+
+BLMPlayer.ActionSet = BLMOpener
+SCHPlayer.ActionSet = SCHOpener
+RDMPlayer.ActionSet = RDMOpener
+MCHPlayer.ActionSet = MCHOpener
+NINPlayer.ActionSet = NINOpener
+DRKPlayer.ActionSet = DRKOpener
+WARPlayer.ActionSet = WAROpener
+WHMPlayer.ActionSet = WHMOpener
+SAMPlayer.ActionSet = SAMOpener
+PLDPlayer.ActionSet = PLDOpener
+GNBPlayer.ActionSet = GNBOpener
+ASTPlayer.ActionSet = ASTOpener
+
 #NinjaPlayer = Ninja(2.5, NINAction, [], [AutoEffect, NinjutsuTimerEffect], Event)
-Event.PlayerList = [GNBPlayer, WARPlayer, PLDPlayer, DRKPlayer] #BLMPlayer, SCHPlayer, RDMPlayer, MCHPlayer ,DRKPlayer,WARPlayer,WHMPlayer,SAMPlayer
+Event.PlayerList = [BLMPlayer, SCHPlayer, RDMPlayer, MCHPlayer ,DRKPlayer,WARPlayer,ASTPlayer,SAMPlayer] #BLMPlayer, SCHPlayer, RDMPlayer, MCHPlayer ,DRKPlayer,WARPlayer,ASTPlayer,SAMPlayer
 Event.SimulateFight(0.01, 100, 20)
 
