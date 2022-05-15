2e2a01ac675d1fcf69ba5b8e0788ead9fdb583eb
Michael Creegan <creegan.mi@gmail.com>
2022-05-15 14:43:36 -0400

    monk gauge update

--- Jobs/Melee/Monk/Monk_Player.py
+++ Jobs/Melee/Monk/Monk_Player.py
@@ -10,34 +10,88 @@ class Monk(Melee):
         super().__init__(GCDTimer, ActionSet, PrePullSet, EffectList, CurrentFight, Stat)
 
         #Gauge
-        self.FifthChakra= 0 #5 meditation chakras for forbidden chakra, steel peak, howling fist, and enlightenment
+        self.FifthChakra = 0 #5 meditation chakras for forbidden chakra, steel peak, howling fist, and enlightenment
+        self.ElixirField = False   #masterful blitz
+        self.CelestialRevolution = False   #masterful blitz
+        self.RisingPhoenix = False #masterful blitz
+        self.PhantomRush = False   #masterful blitz
 
         #Stacks
-        self.RiddleOfEarth = 0
+        self.RiddleOfEarth = 0  #three stacks
         self.LunarNadi = 0
-        self.SolarNadi = 0        
+        self.SolarNadi = 0
+        self.Thunderclap = 0 #three charges
+        self.PerfectBalance = 0 #three stacks       
 
         #Forms
         self.OpoOpoForm = False #if in opo-opo and have leaden fist active, use bootshine else dragon kick
         self.RaptorForm = False #if disciplined fist at 7s or more use true strike else twin snakes
         self.CoeurlForm = False #if demonlish has 4s or less use demolish else snap punch
+        self.FormShift = False
 
         #Buffs
         self.LeadenFist = False  #guaranteed critical hit in opo-opo form for from dragon kick
         self.DisciplinedFist = False #increases damage dealt by 15% for 15s from twin snakes
         self.ShadowOfTheDestroyer = False #guaranteed critical hit in opo-opo form from shadow of the destroyer
         self.RiddleOfFire = False   #increases damage dealt by 15% for 20s
-        self.RiddleOfEarth = False  #reduces auto-attack delay by 50% for 15s
-        self.RiddleOfWind = False   #grants 3 stacks, each stack reduced damage taken by 20%
+        self.RiddleOfEarth = False  #grants 3 stacks, each stack reduced damage taken by 20%
+        self.RiddleOfWind = False   #reduces auto-attack delay by 50% for 15s
         self.Brotherhood = False    #increases damage dealt by 5% for 15s
         self.MeditativeBrotherhood = False  #20% chance open chakra when party members under this effect land weaponskill or cast spell under this effect
         self.Anatman = False    #extends duration of Disciplined Fist and current form to maximum and halts expiration
         self.PerfectBalance = False #for GCD executed skills to open elixir field, celestial revolution, rising phoenix, phantom rush
- 
 
+        #Next Crit
+        self.NextCrit = False #leadenfist
+ 
         #Timer
-        self.DemolishTimer = 0   #demolish dot 18s
+        self.DemolishDOTTimer = 0   #demolish dot 18s
         self.DisciplinedFistTimer = 0   #twin snakes 15s
+        self.LeadenFistTimer = 0 #dragon kick grants guaranteed next crit 30s
+        self.RiddleOfWindTimer = 0
+        self.RiddleOfFireTimer = 0
+        self.BrotherhoodTimer = 0
+        self.PerfectBalanceTimer = 0
+        self.ThunderclapTimer = 0
+        self.MantraTimer = 0
+        self.AnatmanTimer = 0
 
         #DOT
         self.Demolish = None
+
+        #CD
+        self.perfectBalanceCD = 0
+        self.RiddleOfFireCD = 0
+        self.RiddleOfWindCD = 0
+        self.RiddleOfEarthCD = 0
+        self.BrotherhoodCD = 0
+        self.ThunderclapCD = 0
+        self.MantraCD = 0
+        self.AnatmanCD = 0
+
+
+        #JobMod
+        self.JobMod = 110
+
+    def updateCD(self, time):
+        if (self.perfectBalanceCD > 0) : self.perfectBalanceCD = max(0,self.perfectBalanceCD - time)
+        if (self.RiddleOfFireCD > 0) : self.RiddleOfFireCD = max(0,self.RiddleOfFireCD - time)
+        if (self.RiddleOfWindCD > 0) : self.RiddleOfWindCD = max(0,self.RiddleOfWindCD - time)
+        if (self.RiddleOfEarthCD > 0) : self.RiddleOfEarthCD = max(0,self.RiddleOfEarthCD - time)
+        if (self.BrotherhoodCD > 0) : self.BrotherhoodCD  = max(0,self.BrotherhoodCD  - time)
+        if (self.ThunderclapCD > 0) : self.ThunderclapCD = max(0,self.ThunderclapCD - time)
+        if (self.MantraCD > 0) : self.MantraCD = max(0,self.MantraCD - time)
+        if (self.AnatmanCD > 0) : self.AnatmanCD  = max(0,self.AnatmanCD - time)
+
+    def updateTimer(self, time):
+        super().updateTimer(time)
+        if (self.DemolishDOTTimer  > 0) : self.DemolishDOTTimer  = max(0,self.DemolishDOTTimer - time)
+        if (self.DisciplinedFistTimer > 0) : self.DisciplinedFistTimer = max(0,self.DisciplinedFistTimer - time)
+        if (self.LeadenFistTimer > 0) : self.LeadenFistTimer = max(0,self.LeadenFistTimer - time)
+        if (self.RiddleOfWindTimer > 0) : self.RiddleOfWindTimer = max(0,self.RiddleOfWindTimer - time)
+        if (self.RiddleOfFireTimer > 0) : self.RiddleOfFireTimer = max(0,self.RiddleOfFireTimer - time)
+        if (self.BrotherhoodTimer > 0) : self.BrotherhoodTimer = max(0,self.BrotherhoodTimer - time)
+        if (self.PerfectBalanceTimer > 0) : self.PerfectBalanceTimer = max(0,self.PerfectBalanceTimer - time)
+        if (self.ThunderclapTimer > 0) : self.ThunderclapTimer = max(0,self.ThunderclapTimer - time)
+        if (self.MantraTimer > 0) : self.MantraTimer = max(0,self.MantraTimer - time)
+        if (self.AnatmanTimer > 0) : self.AnatmanTimer = max(0,self.AnatmanTimer - time)
