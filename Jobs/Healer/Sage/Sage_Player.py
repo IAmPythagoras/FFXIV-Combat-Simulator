55ad8b5c3148d2ee84cad1273c57d98571418a3a
Anthony Desrochers <anthony.desrochers17@gmail.com>
2022-06-03 12:32:14 -0400

    Added Sage Heals and other mechanics
    Heals do not actually heal :x

--- Jobs/Healer/Sage/Sage_Player.py
+++ Jobs/Healer/Sage/Sage_Player.py
@@ -1,10 +1,14 @@
 from Jobs.Healer.Healer_Player import Healer
-
+from Jobs.Healer.Sage.Sage_Spell import AddersgallCheck
 
 class Sage(Healer):
 
     def __init__(self, GCDTimer, ActionSet, PrePullSet, EffectList, CurrentFight, Stat):
         super().__init__(GCDTimer, ActionSet, PrePullSet, EffectList, CurrentFight, Stat)
+
+        #Stack
+        self.AddersgallStack = 0
+        self.AdderstingStack = 0
 
         #Buff
         self.Eukrasia = False
@@ -12,9 +16,21 @@ class Sage(Healer):
         #CD
         self.PneumaCD = 0
         self.PhlegmaCD = 0
-
+        self.KrasisCD = 0
+        self.PanhaimaCD = 0
+        self.HolosCD = 0
+        self.RhizomataCD = 0
+        self.HaimaCD = 0
+        self.TaurocholeCD = 0
+        self.PepsiCD = 0
+        self.ZoeCD = 0
+        self.IxocholeCD = 0
+        self.KeracholeCD = 0
+        self.IcarusCD = 0
+        self.SoteriaCD = 0
         #Timer
         self.EukrasianTimer = 0
+        self.AddersgallTimer = 40 #Starting at 40 since 20 sec countdown
 
         #DOT
         self.Eukrasian = None
@@ -22,12 +38,27 @@ class Sage(Healer):
         #Stack
         self.PhlegmaStack = 2
 
+        self.EffectCDList.append(AddersgallCheck)
+
 
     def updateCD(self, time):
         super().updateCD(time)
         if (self.PneumaCD > 0) : self.PneumaCD = max(0,self.PneumaCD - time)
         if (self.PhlegmaCD > 0) : self.PhlegmaCD = max(0,self.PhlegmaCD - time)
+        if (self.KrasisCD > 0) : self.KrasisCD = max(0,self.KrasisCD - time)
+        if (self.PanhaimaCD > 0) : self.PanhaimaCD = max(0,self.PanhaimaCD - time)
+        if (self.HolosCD > 0) : self.HolosCD = max(0,self.HolosCD - time)
+        if (self.RhizomataCD > 0) : self.RhizomataCD = max(0,self.RhizomataCD - time)
+        if (self.HaimaCD > 0) : self.HaimaCD = max(0,self.HaimaCD - time)
+        if (self.TaurocholeCD > 0) : self.TaurocholeCD = max(0,self.TaurocholeCD - time)
+        if (self.PepsiCD > 0) : self.PepsiCD = max(0,self.PepsiCD - time)
+        if (self.ZoeCD > 0) : self.ZoeCD = max(0,self.ZoeCD - time)
+        if (self.IxocholeCD > 0) : self.IxocholeCD = max(0,self.IxocholeCD - time)
+        if (self.KeracholeCD > 0) : self.KeracholeCD = max(0,self.KeracholeCD - time)
+        if (self.IcarusCD > 0) : self.IcarusCD = max(0,self.IcarusCD - time)
+        if (self.SoteriaCD > 0) : self.SoteriaCD = max(0,self.SoteriaCD - time)
 
     def updateTimer(self, time):
         super().updateTimer(time)
         if (self.EukrasianTimer > 0) : self.EukrasianTimer = max(0,self.EukrasianTimer - time)
+        if (self.AddersgallTimer > 0) : self.AddersgallTimer = max(0,self.AddersgallTimer - time)
--- Jobs/Healer/Sage/Sage_Spell.py
+++ Jobs/Healer/Sage/Sage_Spell.py
@@ -16,7 +16,95 @@ def PneumaRequirement(Player, Spell):
 def PhlegmaRequirement(Player, Spell):
     return Player.PhlegmaStack > 0, Player.PhlegmaCD
 
+def AddersgallRequirement(Player, Enemy):
+    return Player.AddersgallStack > 0, -1
+
+def KrasisRequirement(Player, Spell):
+    return Player.KrasisCD <= 0, Player.KrasisCD
+
+def PanhaimaRequirement(Player, Spell):
+    return Player.PanhaimaCD <= 0, Player.PanhaimaCD
+
+def HolosRequirement(Player, Spell):
+    return Player.HolosCD <= 0, Player.HolosCD
+
+def RhizomataRequirement(Player, Spell):
+    return Player.RhizomataCD <= 0, Player.RhizomataCD
+
+def HaimaRequirement(Player, Spell):
+    return Player.HaimaCD <= 0, Player.HaimaCD
+
+def TaurocholeRequirement(Player, Spell):
+    return Player.TaurocholeCD <= 0, Player.TaurocholeCD
+
+def PepsiRequirement(Player, Spell):
+    return Player.PepsiCD <= 0, Player.PepsiCD
+
+def ZoeRequirement(Player, Spell):
+    return Player.ZoeCD <= 0, Player.ZoeCD
+
+def IxocholeRequirement(Player, Spell):
+    return Player.IxocholeCD <= 0, Player.IxocholeCD
+
+def KeracholeRequirement(Player, Spell):
+    return Player.KeracholeCD <= 0, Player.KeracholeCD
+
+def IcarusRequirement(Player, Spell):
+    return Player.IcarusCD <= 0, Player.IcarusCD
+
+def SoteriaRequirement(Player, Spell):
+    return Player.SoteriaCD <= 0, Player.SoteriaCD
+
 #Apply
+
+def ApplyDiagnosis(Player, Enemy):
+    Player.AdderstingStack = min(3, Player.AdderstingStack + 1) #Will be assumed each time we do it gives a stack
+
+def ApplyKrasis(Player, Enemy):
+    Player.KrasisCD = 60
+
+def ApplyPanhaima(Player, Enemy):
+    Player.PanhaimaCD = 120
+
+def ApplyHolos(Player, Enemy):
+    Player.HolosCD = 120
+
+def ApplyRhizomata(Player, Enemy):
+    Player.RhizomataCD = 90
+    Player.AddersgallStack = min(3, Player.AddersgallStack + 1)
+
+def Apply(Player, Enemy):
+    Player.CD = 0
+
+def ApplyHaima(Player, Enemy):
+    Player.HaimaCD = 120
+
+def ApplyTaurochole(Player, Enemy):
+    Player.TaurocholeCD = 45
+    Player.AddersgallStack -= 1
+
+def ApplyPepsi(Player, Enemy):
+    Player.PepsiCD = 30
+
+def ApplyZoe(Player, Enemy):
+    Player.ZoeCD = 90
+
+def ApplyKerachole(Player, Enemy):
+    Player.KeracholeCD = 30
+    Player.AddersgallStack -= 1
+
+def ApplyIxochole(Player, Enemy):
+    Player.IxocholeCD = 30
+    Player.AddersgallStack -= 1
+
+def ApplyIcarus(Player, Enemy):
+    Player.IcarusCD = 45
+
+def ApplySoteria(Player, Enemy):
+    Player.SoteriaCD = 90
+
+def ApplyDruochole(Player, Enemy):
+    Player.AddersgallStack -= 1
 
 def ApplyEukrasia(Player, Enemy):
     Player.Eukrasia = True
@@ -40,6 +128,11 @@ def ApplyEukrasian(Player, Enemy):
 
 
 #Check
+
+def AddersgallCheck(Player, Enemy):
+    if Player.AddersgallTimer <= 0:
+        Player.AddersgallStack = min(3, Player.AddersgallStack + 1)
+        Player.AddersgallTimer = 0
 
 def PhlegmaStackCheck(Player, Enemy):
     if Player.PhlegmaTimer <= 0:
@@ -68,7 +161,7 @@ Pneuma = SageSpell(4, True, 1.5, 2.5, 330, 700, ApplyPneuma, [ManaRequirement, P
 #Healing GCD
 Egeiro = SageSpell(1, True, 8, 2.5, 0, 2400, empty, [ManaRequirement])
 Prognosis = SageSpell(1, True, 2, 2.5, 0, 800, empty, [ManaRequirement])
-Diagnosis = SageSpell(1, True, 1.5, 2.5, 0, 400, empty, [ManaRequirement])
+Diagnosis = SageSpell(1, True, 1.5, 2.5, 0, 400, ApplyDiagnosis, [ManaRequirement])
 #Damage oGCD
 Eukrasia = SageSpell(5, False, Lock, 0, 0, 0, ApplyEukrasia, []) #Since only 1 sec CD, no real need to put a requirement
 
@@ -77,11 +170,12 @@ Krasis = SageSpell(1, False, 0, 0, 0, 0, ApplyKrasis, [KrasisRequirement])
 Panhaima = SageSpell(1, False, 0, 0, 0, 0, ApplyPanhaima, [PanhaimaRequirement])
 Holos = SageSpell(1, False, 0, 0, 0, 0, ApplyHolos, [HolosRequirement])
 Rhizomata = SageSpell(1, False, 0, 0, 0, 0, ApplyRhizomata, [RhizomataRequirement])
-Haimi = SageSpell(1, False, 0, 0, 0, 0, ApplyHaima, [HaimaRequirement])
-Taurochole = SageSpell(1, False, 0, 0, 0, 0, ApplyTaurochole, [TaurocholeRequirement])
+Haima = SageSpell(1, False, 0, 0, 0, 0, ApplyHaima, [HaimaRequirement])
+Taurochole = SageSpell(1, False, 0, 0, 0, 0, ApplyTaurochole, [TaurocholeRequirement,AddersgallRequirement])
 Pepsi = SageSpell(1, False, 0, 0, 0, 0, ApplyPepsi, [PepsiRequirement])
 Zoe = SageSpell(1, False, 0, 0, 0, 0, ApplyZoe, [ZoeRequirement]) #https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.leagueoflegends.com%2Fen-pl%2Fchampions%2Fzoe%2F&psig=AOvVaw2X9QcnXQ_CGp3xMY8MLua9&ust=1654295724885000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJjl4oPqj_gCFQAAAAAdAAAAABAD
-Ixochole = SageSpell(1, False, 0, 0, 0, 0, ApplyIxochole, [IxocholeRequirement])
-Kerachole = SageSpell(1, False, 0, 0, 0, 0, ApplyKerachole, [KeracholeRequirement])
+Ixochole = SageSpell(1, False, 0, 0, 0, 0, ApplyIxochole, [IxocholeRequirement,AddersgallRequirement])
+Kerachole = SageSpell(1, False, 0, 0, 0, 0, ApplyKerachole, [KeracholeRequirement,AddersgallRequirement])
 Icarus = SageSpell(1, False, 0, 0, 0, 0, ApplyIcarus, [IcarusRequirement])
 Soteria = SageSpell(1, False, 0, 0, 0, 0, ApplySoteria, [SoteriaRequirement])
+Druochole = SageSpell(1, False, 0, 0, 0, 0, ApplyDruochole, [AddersgallRequirement])
