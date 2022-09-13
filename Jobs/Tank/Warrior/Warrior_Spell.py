a1440b0df6136f830b484d483f3030b2403976b6
Anthony <anthony.desrochers17@gmail.com>
2022-09-13 09:23:58 -0400

    WIP
    WAR and DRK might have problems

--- Jobs/Tank/Warrior/Warrior_Spell.py
+++ Jobs/Tank/Warrior/Warrior_Spell.py
@@ -200,12 +200,12 @@ def InfuriateStackCheck(Player, Enemy):
 #Combo Action
 HeavySwing = WarriorSpell(31, True, Lock, 2.5, 200, 0, ApplyHeavySwing, [],0)
 Maim = WarriorSpell(37, True, Lock, 2.5, 150, 0, ApplyMaim, [],0)
-StormEye = WarriorSpell(45, True, Lock, 2.5, 130, 0, ApplyStormEye, [],0)
-StormPath = WarriorSpell(42, True, Lock, 2.5, 130, 0, ApplyStormPath, [], 0)
+StormEye = WarriorSpell(45, True, Lock, 2.5, 150, 0, ApplyStormEye, [],0)
+StormPath = WarriorSpell(42, True, Lock, 2.5, 150, 0, ApplyStormPath, [], 0)
 #Missing Storm's path
 
 #oGCD
-Upheaval = WarriorSpell(7387, False, Lock, 0, 350, 0, ApplyUpheaval, [UpheavalRequirement],0)
+Upheaval = WarriorSpell(7387, False, Lock, 0, 360, 0, ApplyUpheaval, [UpheavalRequirement],0)
 Onslaught = WarriorSpell(7386, False, Lock, 0, 150, 0, ApplyOnslaught, [OnslaughtRequirement],0)
 Infuriate = WarriorSpell(52, False, Lock, 0, 0, 0, ApplyInfuriate, [InfuriateRequirement],0)
 InnerRelease = WarriorSpell(7389, False, Lock, 0, 0, 0, ApplyInnerRelease, [InnerReleaseRequirement],0)
@@ -213,7 +213,7 @@ InnerRelease = WarriorSpell(7389, False, Lock, 0, 0, 0, ApplyInnerRelease, [Inne
 
 #GCD
 PrimalRend = WarriorSpell(25753, True, Lock, 2.5, 700, 0, ApplyPrimalRend, [PrimalRendRequirement],0)
-FellCleave = WarriorSpell(3549, True, Lock, 2.5, 460, 0, empty, [],50)
+FellCleave = WarriorSpell(3549, True, Lock, 2.5, 470, 0, empty, [],50)
 InnerChaos = WarriorSpell(16465, True, Lock, 2.5, 650,0, ApplyInnerChaos, [InnerChaosRequirement], 50)
 Tomahawk = WarriorSpell(46, True, Lock, 2.5, 150, 0, empty, [], 0)
 
