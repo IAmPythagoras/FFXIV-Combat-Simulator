35a6d1b51e1a80486450a1bbc45610c66c4a60c3
Anthony Desrochers <anthony.desrochers17@gmail.com>
2022-11-08 22:11:49 -0500

    d

--- ffxivcalc/__main__.py
+++ ffxivcalc/__main__.py
@@ -20,7 +20,7 @@ def get_parser() -> ArgumentParser:
     parser.add_argument('-l', '--time-limit', type=int, default=1000)
 
     # Running tester
-    parser.add_argument('-run_tests', '--run_tests', type=None, default=None)
+    parser.add_argument('-run_tests', '--run_tests', type=bool, default=False)
 
     # Running tester
     parser.add_argument('-TUI', '--TUI_open', type=bool, default=False)
