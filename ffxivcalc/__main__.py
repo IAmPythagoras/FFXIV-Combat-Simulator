871f18f0437a332f2c65be435c49b27ea52b820b
Anthony Desrochers <anthony.desrochers17@gmail.com>
2022-12-12 16:06:19 -0500

    merged https://github.com/IAmPythagoras/FFXIV-Combat-Simulator/pull/20

--- ffxivcalc/__main__.py
+++ ffxivcalc/__main__.py
@@ -2,18 +2,21 @@
 from argparse import ArgumentParser
 from pathlib import Path
 from sys import exit
+import logging
 
 from ffxivcalc import __name__ as prog
 from ffxivcalc.UI.SimulationInput import ExecuteMemoryCode as fight_main
 from ffxivcalc.Tester.Tester import Tester
 from ffxivcalc.UI.TUI import TUI_draw
 
+__logger__ = logging.getLogger("ffxivcalc") # root logger
+
 def get_parser() -> ArgumentParser:
     """Defines all the cli arguments to be parsed
-
     :return: An ArgumentParser object
     """
     parser = ArgumentParser(prog=prog)
+    parser.add_argument('-v', '--verbose', action='count', default=0)
     subparsers = parser.add_subparsers(help='action to perform', dest='action')
 
     # Running in code simulation
@@ -35,12 +38,23 @@ def get_parser() -> ArgumentParser:
 def main() -> int:
     """
     Entrypoint into the cli. Can be called with ``python -mffxivcalc``
-
     :return: Return code, for use with cli programs to indicate success/failure
     """
     parser = get_parser()
     args = parser.parse_args()
-    #input(args.json)
+
+    if args.verbose > 0:
+        match args.verbose:
+            case 1:
+                level = logging.ERROR
+            case 2:
+                level = logging.INFO
+            case 3:
+                level = logging.DEBUG
+            case _: # more than 3 V is too much
+                level = logging.DEBUG
+        logging.basicConfig(format='[%(levelname)s] %(message)s')
+        __logger__.setLevel(level=level)
 
     match args.action:
         case 'simulate':
