5450e4a5739c09d319d8030c48d9fbefd5c815ba
Anthony Desrochers <anthony.desrochers17@gmail.com>
2023-05-03 00:13:32 -0400

    Removing AA's potency

--- ffxivcalc/Jobs/Base_Spell.py
+++ ffxivcalc/Jobs/Base_Spell.py
@@ -320,8 +320,8 @@ class Auto_Attack(DOTSpell):
         """
         Ranged : bool -> True if the auto is ranged.
         """
-        if Ranged : super().__init__(id, 80, True) # Ranged AA, 80 potency
-        else: super().__init__(id, 90, True) # Melee AA, 90 potency
+        if Ranged : super().__init__(id, 0, True) # Ranged AA, 80 potency
+        else: super().__init__(id, 0, True) # Melee AA, 90 potency
 
         self.DOTTimer = 0 
 
