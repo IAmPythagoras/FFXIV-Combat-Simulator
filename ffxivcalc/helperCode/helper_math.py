037a0d6cf83079b8f6b1a429b3b3d57b3551a0f8
Anthony Desrochers <anthony.desrochers17@gmail.com>
2024-01-05 13:36:38 -0500

    removed print

--- ffxivcalc/helperCode/helper_math.py
+++ ffxivcalc/helperCode/helper_math.py
@@ -4,7 +4,6 @@ def isclose(a, b, rel_tol=1e-09, abs_tol=0.0): # Helper function to compare floa
     return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
 def roundDown(x, precision):
-    print("hello : " + str(x))
     return math.floor(x * 10**precision)/10**precision
 
 def roundUp(x, precision):
