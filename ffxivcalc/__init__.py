ab4fa86bc614138eb6e75d0e2fcdfb17da9ae00a
Anthony Desrochers <anthony.desrochers17@gmail.com>
2024-01-24 22:37:22 -0500

    Updating Update history for v0.8.800

--- ffxivcalc/__init__.py
+++ ffxivcalc/__init__.py
@@ -3,6 +3,15 @@ __version__ = '0.8.800'
 """
 Update history :
 
+0.8.800 :
+    - Made changes to work with web app
+    - Added ways to remove pB or redirect the output
+    - Fixed etro import issues
+    - BiSSolver now makes a deepcopy of the gear space so it can be reused
+    - Fixed pb issues, fixed fight import issues.
+    - Added 'ShowProgress' option to 'SimulateFight()' that lets enables the print of the loading bar
+    - Various other bug fixes
+
 0.8.700 :
     - Made PyPi package.
 
